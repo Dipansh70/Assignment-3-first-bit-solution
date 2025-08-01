bacaa6589b1fe3dd0285239a8d9c462b0ca2f619
Dipanshu Sharma <dipanshusharma7015@gmail.com>
2025-08-02 01:34:03 +0530

    Question 5. = Answer

---
+++ loop5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+	int no = 153;
+	int temp = no;
+	int rem , sum = 0;
+	
+	 while(no>0){
+	 	rem = no % 10;
+	 	sum = sum + rem * rem * rem;
+        no = no / 10;
+	 }
+	  
+	if(temp == sum) {
+        printf("Armstrong");
+    } else {
+        printf("Not Armstrong");
+    }
+}
